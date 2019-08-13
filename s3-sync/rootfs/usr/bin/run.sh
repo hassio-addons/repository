d695d52ca7d9c4f881d984ed7109a2cbaf7fb352
zer0 <steve.luther@gmail.com>
2019-08-12 22:34:30 -0700

    add some more debugging to the aws command

--- s3-sync/rootfs/usr/bin/run.sh
+++ s3-sync/rootfs/usr/bin/run.sh
@@ -11,6 +11,7 @@ export AWS_SECRET_ACCESS_KEY=$(bashio::config 'aws_secret_key')
 
 # Bucket to Sync to 
 BUCKET=$(bashio::config 'aws_s3_bucket')
+LOG_LEVEL=$(bashio::config 'log_level')
 
 bashio::log.debug "Find Backup Directory"
 bashio::log.debug "Found: $(cd / && find . -type d -name 'backup')"
@@ -19,7 +20,14 @@ bashio::log.debug "Found: $(cd / && find . -type d -name 'backup')"
 # Error if no internet or something
 bashio::log.debug "Running command: aws s3 sync /backup/ s3://$BUCKET/"
 # This is extremely noise in the logs. Also, maybe only get the most recent?
-aws s3 sync /backup/ s3://$BUCKET/ --only-show-errors
+
+if [ $LOG_LEVEL = 'debug' ]; then
+  aws s3 sync /backup/ s3://$BUCKET/
+elif [ $LOG_LEVEL = 'info' ]; then
+  aws s3 sync /backup/ s3://$BUCKET/ --only-show-errors
+else
+  aws s3 sync /backup/ s3://$BUCKET/ --only-show-errors
+fi
 
 bashio::log.info "Move Complete"
 bashio::log.info "Bucket Contents: $(aws s3 ls s3://$BUCKET)"
